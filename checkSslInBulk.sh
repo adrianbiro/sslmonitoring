3e1cad124bde2217af68df8647c5969339048b0b
Adrián Bíro <biroadrian@protonmail.com>
2022-11-30 11:16:31 +0100

    small fixes

--- checkSslInBulk.sh
+++ checkSslInBulk.sh
@@ -1,5 +1,7 @@
 for i in $(awk -F, 'NR>1{print $2}' *.csv);
 do
   echo "${i}: $(./sslmonitoring ${i})"
+  # just return number not a error
+  #echo "${i}: $(/home/adrian/gits/m_bin/,checkssl ${i})"
 #  echo "${i},$(./sslmonitoring ${i})"
 done
